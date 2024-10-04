43fa1156d7d458475a422b55b4fbb85c14d0c386
spotsonthestove <git@stackblitz.com>
2024-10-05 09:11:25 +1300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Threes from '$lib/Threes.svelte';
+  import BranchAdder from '$lib/BranchAdder.svelte';
+</script>
+
+<main>
+  <h1>Mind Map</h1>
+  <div class="mind-map-container">
+    <Threes />
+    <BranchAdder />
+  </div>
+</main>
+
+<style>
+  .mind-map-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-top: 2rem;
+  }
+</style>
